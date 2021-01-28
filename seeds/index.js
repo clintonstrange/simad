26e73aa4164e067103d373d57152f09c0d0b74af
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-01-27 20:27:03 -0600

    updated controller routes

---
+++ seeds/index.js
@@ -0,0 +1,22 @@
+const seedCategories = require("./category-seeds");
+const seedProducts = require("./product-seeds");
+const seedUsers = require("./user-seeds");
+
+const sequelize = require("../config/connection");
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log("\n----- DATABASE SYNCED -----\n");
+  await seedCategories();
+  console.log("\n----- CATEGORIES SEEDED -----\n");
+
+  await seedProducts();
+  console.log("\n----- PRODUCTS SEEDED -----\n");
+
+  await seedUsers();
+  console.log("\n----- USERS SEEDED -----\n");
+
+  process.exit(0);
+};
+
+seedAll();
