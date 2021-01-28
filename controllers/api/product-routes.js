ab5d5225a11637d0f1c9d12bf6210d36fd5d2fd5
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-01-28 17:00:52 -0600

    added update product

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -58,6 +58,21 @@ router.post("/", (req, res) => {
     });
 });
 
+// update product
+router.put("/:id", (req, res) => {
+  // update product data
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => res.json(product))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
   Product.destroy({
